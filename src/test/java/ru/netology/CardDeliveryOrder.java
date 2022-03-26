db63df261e956ed76630c26ed661011245582db2
Mikhail Voroshilov <m.s.voroshilovv@gmail.com>
2022-03-26 19:40:22 +0800

    изменен стиль кода

--- src/test/java/ru/netology/CardDeliveryOrder.java
+++ src/test/java/ru/netology/CardDeliveryOrder.java
@@ -60,14 +60,15 @@ public class CardDeliveryOrder {
 
     @ParameterizedTest
     @CsvSource({
-            "English, Moskva",
-            "spec Simbol, Москв@",
-            "underscore, Улан_удэ",
-            "space, Горно Алтайск",
-            "not available, Братск",
-            "typo, Налчик",
+            "English, Moskva, Доставка в выбранный город недоступна",
+            "spec Simbol, Москв@, Доставка в выбранный город недоступна",
+            "underscore, Улан_удэ, Доставка в выбранный город недоступна",
+            "space, Горно Алтайск, Доставка в выбранный город недоступна",
+            "not available, Братск, Доставка в выбранный город недоступна",
+            "typo, Налчик, Доставка в выбранный город недоступна",
+            "null,'', Поле обязательно для заполнения",
     })
-    public void shouldTestIncorrectEnterCity(String text, String name) {
+    public void shouldTestIncorrectEnterCity(String text, String name, String error) {
         $$x("//input[@placeholder='Город']").exclude(hidden).first().setValue(name);
 //        exclude-исключает (hidden)- не видемые first()-используем первый из оставшихся
         $("[data-test-id='date'] input").sendKeys( Keys.CONTROL +"A",Keys.DELETE);
@@ -78,31 +79,33 @@ public class CardDeliveryOrder {
         $(".button").click();
 
         $("[data-test-id=city].input_invalid .input__sub").shouldBe(visible);
-        $$x("//*[contains(text(),'Доставка в выбранный город недоступна')]").filter(visible).last().shouldBe(visible); // не работает при пустом значении- другой текст "Поле обязательно для заполнения"
+//        $$x("//*[contains(text(),'Доставка в выбранный город недоступна')]").filter(visible).last().shouldBe(visible); // не работает при пустом значении- другой текст "Поле обязательно для заполнения"
 //    filter- фильтруем по (visible)- видимым на странице last()-выбираем последний элемент по отфильтрованным
+        $(withText(error)).shouldHave(visible, Duration.ofSeconds(15));
     }
 
-    @Test
-    public void shouldTestEnterNullCity() {
-        $x("//input[@placeholder='Город']").setValue("");
-        $("[data-test-id='date'] input").sendKeys( Keys.CONTROL +"A",Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(String.valueOf(date));
-        $("[data-test-id='name'] input").setValue("Анна-петровна");
-        $("[data-test-id='phone'] input").setValue("+79305698778");
-        $("[data-test-id=agreement]").click();
-        $(".button").click();
-
-//        $("[data-test-id=city].input_invalid .input__sub").shouldBe(visible);
-        $x("//*[contains(text(),'Поле обязательно для заполнения')]").shouldBe(visible); // не работает при пустом значении- другой текст "Поле обязательно для заполнения"
-    }
+//    @Test
+//    public void shouldTestEnterNullCity() {
+//        $x("//input[@placeholder='Город']").setValue("");
+//        $("[data-test-id='date'] input").sendKeys( Keys.CONTROL +"A",Keys.DELETE);
+//        $("[data-test-id='date'] input").setValue(String.valueOf(date));
+//        $("[data-test-id='name'] input").setValue("Анна-петровна");
+//        $("[data-test-id='phone'] input").setValue("+79305698778");
+//        $("[data-test-id=agreement]").click();
+//        $(".button").click();
+//
+////        $("[data-test-id=city].input_invalid .input__sub").shouldBe(visible);
+//        $x("//*[contains(text(),'Поле обязательно для заполнения')]").shouldBe(visible); // не работает при пустом значении- другой текст "Поле обязательно для заполнения"
+//    }
 
     @ParameterizedTest
     @CsvSource({
-            "English, Maik",
-            "spec Simbol, Петр Машк&вич",
-            "underscore, Анна_Сергеевна",
+            "English, Maik, Имя и Фамилия указаные неверно.",
+            "spec Simbol, Петр Машк&вич, Имя и Фамилия указаные неверно.",
+            "underscore, Анна_Сергеевна, Имя и Фамилия указаные неверно.",
+            "null,'', Поле обязательно для заполнения",
     })
-    public void shouldTestIncorrectEnterName(String text, String name) {
+    public void shouldTestIncorrectEnterName(String text, String name, String error) {
         $("[data-test-id='city'] input").setValue("Иркутск");
         $("[data-test-id='date'] input").sendKeys( Keys.CONTROL +"A",Keys.DELETE);
         $("[data-test-id='date'] input").setValue(String.valueOf(date));
@@ -112,32 +115,34 @@ public class CardDeliveryOrder {
         $(".button").click();
 
         $("[data-test-id=name].input_invalid .input__sub").shouldBe(visible);
-        $x("//*[contains(text(),'Имя и Фамилия указаные неверно.')]").shouldBe(visible); // не работает при пустом значении- другой текст "Поле обязательно для заполнения"
+//        $x("//*[contains(text(),'Имя и Фамилия указаные неверно.')]").shouldBe(visible); // не работает при пустом значении- другой текст "Поле обязательно для заполнения"
+        $(withText(error)).shouldHave(visible);
     }
 
-    @Test
-    public void shouldTestEnterNullName() {
-        $("[data-test-id='city'] input").setValue("Иркутск");
-        $("[data-test-id='date'] input").sendKeys( Keys.CONTROL +"A",Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(String.valueOf(date));
-        $("[data-test-id='name'] input").setValue("");
-        $("[data-test-id='phone'] input").setValue("+79305698778");
-        $("[data-test-id=agreement]").click();
-        $(".button").click();
-
-//        $("[data-test-id=name].input_invalid .input__sub").shouldBe(visible);
-        $x("//*[contains(text(),'Поле обязательно для заполнения')]").shouldBe(visible); // не работает при пустом значении- другой текст "Поле обязательно для заполнения"
-    }
+//    @Test
+//    public void shouldTestEnterNullName() {
+//        $("[data-test-id='city'] input").setValue("Иркутск");
+//        $("[data-test-id='date'] input").sendKeys( Keys.CONTROL +"A",Keys.DELETE);
+//        $("[data-test-id='date'] input").setValue(String.valueOf(date));
+//        $("[data-test-id='name'] input").setValue("");
+//        $("[data-test-id='phone'] input").setValue("+79305698778");
+//        $("[data-test-id=agreement]").click();
+//        $(".button").click();
+//
+////        $("[data-test-id=name].input_invalid .input__sub").shouldBe(visible);
+//        $x("//*[contains(text(),'Поле обязательно для заполнения')]").shouldBe(visible); // не работает при пустом значении- другой текст "Поле обязательно для заполнения"
+//    }
 
     @ParameterizedTest
     @CsvSource({
-            "not plus, 79025647891",
-            "over limit, +798541236547",
-            "less than limit, +7984562356",
-            "spec simbol, +7&963256485",
-            "text phone number, +7e965463269",
+            "not plus, 79025647891, Телефон указан неверно.",
+            "over limit, +798541236547, Телефон указан неверно.",
+            "less than limit, +7984562356, Телефон указан неверно.",
+            "spec simbol, +7&963256485, Телефон указан неверно.",
+            "text phone number, +7e965463269, Телефон указан неверно.",
+            "null,'',Поле обязательно для заполнения",
     })
-    public void shouldTestIncorrectEnterPhone(String text, String phone) {
+    public void shouldTestIncorrectEnterPhone(String text, String phone, String error) {
         $("[data-test-id='city'] input").setValue("Иркутск");
         $("[data-test-id='date'] input").sendKeys( Keys.CONTROL +"A",Keys.DELETE);
         $("[data-test-id='date'] input").setValue(String.valueOf(date));
@@ -147,22 +152,23 @@ public class CardDeliveryOrder {
         $(".button").click();
 
         $("[data-test-id=phone].input_invalid .input__sub").shouldBe(visible);
-        $x("//*[contains(text(),'Телефон указан неверно.')]").should(visible); // не работает при пустом значении- другой текст "Поле обязательно для заполнения"
+//        $x("//*[contains(text(),'Телефон указан неверно.')]").should(visible); // не работает при пустом значении- другой текст "Поле обязательно для заполнения"
+        $(withText(error)).shouldHave(visible);
     }
 
-    @Test
-    public void shouldTesttEnterNullPhone() {
-        $("[data-test-id='city'] input").setValue("Иркутск");
-        $("[data-test-id='date'] input").sendKeys( Keys.CONTROL +"A",Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(String.valueOf(date));
-        $("[data-test-id='name'] input").setValue("Аннф-Петровна");
-        $("[data-test-id='phone'] input").setValue("");
-        $("[data-test-id=agreement]").click();
-        $(".button").click();
-
-//        $("[data-test-id=phone].input_invalid .input__sub").shouldBe(visible);
-        $x("//*[contains(text(),'Поле обязательно для заполнения')]").shouldBe(visible); // не работает при пустом значении- другой текст "Поле обязательно для заполнения"
-    }
+//    @Test
+//    public void shouldTesttEnterNullPhone() {
+//        $("[data-test-id='city'] input").setValue("Иркутск");
+//        $("[data-test-id='date'] input").sendKeys( Keys.CONTROL +"A",Keys.DELETE);
+//        $("[data-test-id='date'] input").setValue(String.valueOf(date));
+//        $("[data-test-id='name'] input").setValue("Аннф-Петровна");
+//        $("[data-test-id='phone'] input").setValue("");
+//        $("[data-test-id=agreement]").click();
+//        $(".button").click();
+//
+////        $("[data-test-id=phone].input_invalid .input__sub").shouldBe(visible);
+//        $x("//*[contains(text(),'Поле обязательно для заполнения')]").shouldBe(visible); // не работает при пустом значении- другой текст "Поле обязательно для заполнения"
+//    }
 
     @Test
     public void shouldNotClickCheckbox() {
